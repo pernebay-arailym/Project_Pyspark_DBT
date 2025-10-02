1464c9505c7d0e75a3d22992b620bf38fb3c68e0
pernebay-arailym <pernebayarailym@gmail.com>
2025-10-02 10:57:07 +0200

    feat: :sparkles: wrote script for the first question metier / found monthly income

---
+++ ventes_dbt/models/queries/analyse_temporelle.sql
@@ -0,0 +1,16 @@
+-- Quel est le chiffre d’affaires mensuel de la boutique ? (l’ensemble de l’entreprise)
+
+select sum(total_amount) as monthly_revenue, year_month
+from {{ref('fact_ventes')}} f
+join {{ref('dim_temps')}} t
+on f.purchase_date = t.purchase_date
+group by year_month
+order by year_month 
+
+
+
+
+
+-- Quels sont les mois les plus rentables de l’année ? (Faire Top 3)
+
+-- Quelle est l’évolution du panier moyen (montant_total / transaction) au fil du temps ?
